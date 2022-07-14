6b0348296b56c048a1056837c1d52cfce9fe93c1
Salim Kachemela <sakachemela@gmail.com>
2022-07-14 16:03:27 +0300

    Fix errors with some output

--- 11-pchar.c
+++ 11-pchar.c
@@ -1,31 +1,21 @@
 #include "monty.h"
-/**
- * f_pchar - prints the char at the top of the stack,
- * followed by a new line
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pchar(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
 
-	h = *head;
-	if (!h)
-	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	if (h->n > 127 || h->n < 0)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%c\n", h->n);
+/**
+  * pchar - Prints the char at the top of the stack,
+  * followed by a new line
+  * @stack: The head of the stack
+  * @line_number: The line on which the error occurred
+  *
+  * Return: Nothing
+  *
+  */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+		handle_error(ERR_PCH_EMP, NULL, line_number, NULL);
+
+	if ((*stack)->n >= 0 && (*stack)->n <= 127)
+		printf("%c\n", (*stack)->n);
+	else
+		handle_error(ERR_PCH_USG, NULL, line_number, NULL);
 }
