f26516ee6b4c67cbff20952629b27c616ebdee5a
Salim Kachemela <sakachemela@gmail.com>
2022-07-14 15:53:12 +0300

    Fix some output errors

--- 1-pint.c
+++ 1-pint.c
@@ -1,19 +1,17 @@
 #include "monty.h"
+
 /**
- * f_pint - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_pint(stack_t **head, unsigned int counter)
+ * pint - Prints the value at the top of the stack,
+ * followed by a new line.
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: Nothing
+ */
+void pint(stack_t **stack, unsigned int line_number)
 {
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	printf("%d\n", (*head)->n);
+	if (*stack == NULL)
+		handle_error(ERR_PINT_USG, NULL, line_number, NULL);
+
+	printf("%d\n", (*stack)->n);
 }
